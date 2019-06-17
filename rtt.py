0076d2f3f661f37cbe43349a4a12907a99702837
Marcos Henrique Curtale Serafim <Marcos.Henrique@schumann.com.br>
2019-06-17 17:02:46 -0300

    Added Timeout interval calculation and DevRTT

--- rtt.py
+++ rtt.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 
 RTTs = np.array([])
 RTTsMean = np.array([])
+DevRTTMean = np.array([])
 a = 0.125
+b = 0.25
 
 def find_roundtriptime(url):
    initial_time = time.time()       #Store the time when request is sent
@@ -16,7 +18,7 @@ def find_roundtriptime(url):
    print('The Round Trip Time for {} is {}'.format(url, str(elapsed_time)))
    return elapsed_time
 
-for i in range(10):
+for i in range(200):
     RTTs = np.append(RTTs, find_roundtriptime(sys.argv[1]))
 
 EstimatedRTT = np.mean(RTTs)
@@ -24,9 +26,20 @@ EstimatedRTT = np.mean(RTTs)
 for i in range(RTTs.shape[0]):
     EstimatedRTT = (1 - a) * EstimatedRTT + a * RTTs[i]
 
+DevRTT = np.mean(RTTs)
+
+for i in range(RTTs.shape[0]):
+    DevRTT = (1 - b) * DevRTT + b * abs(RTTs[i] - EstimatedRTT)
+
+TimeoutInterval = EstimatedRTT + 4 * DevRTT
+
 for i in range(RTTs.shape[0]):
     RTTsMean = np.append(RTTsMean, EstimatedRTT)
 
-plt.plot(RTTs, 'b-')
-plt.plot( RTTsMean, 'r-')
+print("Media Movel Ponderada: " + str(RTTsMean[0]))
+print("Desvio Padrao: " + str(DevRTT))
+print("Intervalo de timeout: " + str(TimeoutInterval))
+
+plt.plot(RTTs, 'b-') # Amostras
+plt.plot(RTTsMean, 'r-') # Media Movel Ponderada
 plt.show()
