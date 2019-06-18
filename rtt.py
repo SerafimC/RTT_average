67d0637598057491f2dc045e79837b56fb289207
Marcos Henrique Curtale Serafim <Marcos.Henrique@schumann.com.br>
2019-06-18 11:31:47 -0300

    Changed calculations

--- rtt.py
+++ rtt.py
@@ -30,14 +30,18 @@ for i in range(RTTs.shape[0]):
 DevRTT = np.mean(RTTs)
 
 for i in range(RTTs.shape[0]):
-    DevRTT = (1 - b) * DevRTT + b * abs(RTTs[i] - EstimatedRTT)
+    DevRTT = (1 - b) * DevRTT + b * abs(RTTs[i] - RTTsMean[i])
+    DevRTTMean = np.append(DevRTTMean, DevRTT)
 
-TimeoutInterval = EstimatedRTT + 4 * DevRTT
+TimeoutInterval = np.mean(EstimatedRTT) + 4 * np.mean(DevRTT)
     
-print("Media Movel Ponderada: " + str(RTTsMean[0]))
+print("Media Movel Ponderada: " + str(np.mean(RTTsMean)))
 print("Desvio Padrao: " + str(DevRTT))
 print("Intervalo de timeout: " + str(TimeoutInterval))
 
 plt.plot(RTTs, 'b-') # Amostras
 plt.plot(RTTsMean, 'r-') # Media Movel Ponderada
+plt.show()
+
+plt.plot(DevRTTMean, 'g-') # Desvio Padrao
 plt.show()
